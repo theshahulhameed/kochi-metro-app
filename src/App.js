6c344d1e4a3cbe86399cc0f368e8a3ccc2e22156
Shahul Hameed <beingshahul@gmail.com>
2019-06-29 13:17:30 +0530

    Installed Bulba, React-Router, React-Router-DOM and configured the App.js file.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import "bulma/css/bulma.css";
+
+import Navbar from "./Navbar";
+import Home from "./components/Home";
+import Stations from "./components/Stations";
+import Fare from "./components/Fare";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Navbar />
+        <div>
+          <Route exact path="/" component={Home} />
+          <Route path="/stations" component={Stations} />
+          <Route path="/fare" component={Fare} />
+        </div>
+      </Router>
     </div>
   );
 }
