2c695cc2f48064e5ce8462e3d1532e8ce6643d91
Shahul Hameed <beingshahul@gmail.com>
2019-06-29 13:29:01 +0530

    Added a homepage with Hero header.

---
+++ src/components/Home.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Home() {
+  return <div>
+                <section className="hero is-medium is-success is-bold">
+                    <div className="hero-body">
+                    <div className="container">
+                        <h1 className="title">
+                            Kochi Metro Application
+                        </h1>
+                        <h2 className="subtitle">
+                            Pricing & Station Information
+                        </h2>
+                    </div>
+                    </div>
+                </section>
+        </div>
+}
+
+export default Home;
---
+++ src/components/Stations.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Stations() {
+  return <p>Stations</p>;
+}
+
+export default Stations;
