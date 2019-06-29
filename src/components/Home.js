3c0d8c3fed5199639bdd371b189eb232a8f51c3c
Shahul Hameed <beingshahul@gmail.com>
2019-06-29 14:02:32 +0530

    Added a footer section.

--- src/components/Home.js
+++ src/components/Home.js
@@ -69,6 +69,11 @@ function Home() {
                     </div>
                 </form>
             </section>
+                <footer className="footer">
+                    <div className="content has-text-centered">
+                        <p>Website by <strong>Shahul Hameed.</strong> </p>
+                    </div>
+                </footer>
         </main>
     )
 }
